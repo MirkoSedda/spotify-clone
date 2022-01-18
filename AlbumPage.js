b5d0d8679402a4a4b72507b868619f358dc80658
BertoPolo <bertopolo91@gmail.com>
2022-01-18 12:20:24 +0100

    fixed problems and creating classes to anime

---
+++ AlbumPage.js
@@ -0,0 +1,15 @@
+const numToPlay = function(){
+    numNode= document.querySelectorAll("li::before")
+    for(let i =0;i<numNode.lengh;i++){
+        numNode[i]
+    }
+}   
+
+// numToPlay()
+
+// onmouseover , , add like heart icon with margin right, add three dots icon with margin left
+
+// highlight : add .highLighting
+// song number : remover innertext and add .listPlay 
+//
+//
