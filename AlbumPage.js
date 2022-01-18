f708ac8c5dd1d5a358d81a56283c1303461a56b7
BertoPolo <bertopolo91@gmail.com>
2022-01-18 14:11:31 +0100

    Update AlbumPage.js

--- AlbumPage.js
+++ AlbumPage.js
@@ -2,16 +2,17 @@ const songHighliter = function(){
     songNode= document.getElementsByClassName("song")
     for(let i =0;i<songNode.length;i++){
         songNode[i].addEventListener("mouseover",function(e){
-            songNode.classList.toggle("highLighting")
+            songNode[i].classList.add("highLighting")
         })
     }
+    
 }   
 
  songHighliter()
 
 // onmouseover , , add like heart icon with margin right, add three dots icon with margin left
 
-// highlight : add .highLighting
+// highlight : add .highLighting  // on mouseleft   with IF ???
 // song number : remover innertext and add .listPlay 
 //
 //
