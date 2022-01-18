3b53cf7b51dd9f09176ede234f39bbd9c8eb57c7
BertoPolo <bertopolo91@gmail.com>
2022-01-18 15:28:54 +0100

    solving JS problem

--- AlbumPage.js
+++ AlbumPage.js
@@ -1,8 +1,19 @@
 const songHighliter = function(){
     songNode= document.getElementsByClassName("song")
+    numNode = document.getElementsByClassName("listNumber")
     for(let i =0;i<songNode.length;i++){
         songNode[i].addEventListener("mouseover",function(e){
             songNode[i].classList.add("highLighting")
+            numNode[i].innerText = " "
+            numNode[i].classList.add("listPlay")
+        })
+    }
+    
+    for(let i =0;i<songNode.length;i++){
+        songNode[i].addEventListener("mouseleave",function(e){
+            songNode[i].classList.toggle("highLighting")
+            numNode[i].innerText = i+1
+            numNode[i].classList.toggle("listPlay")
         })
     }
     
@@ -12,7 +23,7 @@ const songHighliter = function(){
 
 // onmouseover , , add like heart icon with margin right, add three dots icon with margin left
 
-// highlight : add .highLighting  // on mouseleft   with IF ???
+// highlight : add .highLighting  // on mouseleft    hide inner
 // song number : remover innertext and add .listPlay 
 //
 //
