42e53c144a6d41746fb381f9838eb5ead8ab13bd
BertoPolo <bertopolo91@gmail.com>
2022-01-19 15:44:31 +0100

    maaaaadness

--- ArtistPage.js
+++ ArtistPage.js
@@ -5,8 +5,9 @@ const mouseoverSong = function(){
         songNode[i].addEventListener("mouseover",function(e){
             songNode[i].classList.add("highLighting")
             numNode[i].innerText = " "
-            numNode[i].classList.add("listPlay")
-
+            /* numNode[i].classList.add("listPlay") */
+            numNode[i].innerHTML= `<span><i class="bi bi-caret-right-fill"></i></span>`
+            
         })
     }
     
@@ -14,7 +15,8 @@ const mouseoverSong = function(){
         songNode[i].addEventListener("mouseleave",function(e){
             songNode[i].classList.toggle("highLighting")
             numNode[i].innerText = i+1
-            numNode[i].classList.toggle("listPlay")
+            /* numNode[i].classList.toggle("listPlay") */
+            numNode[i].innerHTML= `<span>1</span>`
         })
 
     }
