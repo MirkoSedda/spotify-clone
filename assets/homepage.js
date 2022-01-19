08091033fdd1edb31f21a23ef4a2632459efbb8d
yourusername <kunay69@hotmail.it>
2022-01-19 11:38:22 +0100

    kinda fixed the page

---
+++ assets/homepage.js
@@ -0,0 +1,439 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Heebo:wght@400;500;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="homepage.css" />
+
+    <title>Spotify clone</title>
+  </head>
+  <body>
+    <!-- container fluid main page section -->
+    <main>
+      <div class="container-fluid">
+        <!-- sidebar section -->
+        <div class="row">
+          <div class="col-2 m-sidebar text-white">
+            <aside class="d-flex flex-column">
+              <div class="d-flex mt-1">
+                <i class="bi bi-spotify m-bi-spotify"></i>
+                <span
+                  class="text-1-three-rem fw-bold ms-1 d-flex justify-content-center align-items-center"
+                  >Spotify</span
+                >
+              </div>
+              <div class="d-flex mt-4">
+                <i class="bi bi-house-fill m-bi-house-fill"></i>
+                <span
+                  class="fw-bold ms-1 m-text-small d-flex justify-content-center align-items-center"
+                  >Home</span
+                >
+              </div>
+              <div class="d-flex mt-1">
+                <i class="bi bi-search m-bi-search mid-gray-text"></i>
+                <span
+                  class="ms-1 mid-gray-text m-text-small d-flex justify-content-center align-items-center"
+                  >Search</span
+                >
+              </div>
+              <div class="d-flex align-items-center mt-1 mid-gray-text">
+                <svg
+                  fill="white"
+                  width="16px"
+                  height="16px"
+                  version="1.1"
+                  id="Layer_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 491.52 491.52"
+                  style="enable-background: new 0 0 491.52 491.52"
+                  xml:space="preserve"
+                >
+                  <g>
+                    <g>
+                      <g>
+                        <polygon
+                          points="225.28,409.6 225.28,491.52 266.24,491.52 266.24,409.6 245.76,409.6 			"
+                        />
+                        <path
+                          d="M204.8,225.28h-61.44H81.92H0v143.36h61.44v-102.4c0-11.305,9.155-20.48,20.48-20.48h61.44
+			                	c11.325,0,20.48,9.175,20.48,20.48v102.4h40.96h26.378c1.352-6.697,1.352-13.783,0-20.48h-5.898
+			                	c-7.107,0-13.681-3.686-17.428-9.708c-3.727-6.042-4.076-13.578-0.901-19.927c6.472-12.902,6.472-30.228,0-43.131
+			                	c-3.174-6.349-2.826-13.885,0.901-19.927c3.748-6.021,10.322-9.708,17.428-9.708h161.28c24.392,0,43.52,22.487,43.52,51.2
+			                	c0,5.816-0.963,11.264-2.417,16.425c13.681,8.54,22.897,25.19,22.897,45.015c0,3.523-0.307,6.943-0.86,10.24h41.82V225.28H266.24
+			                	H204.8z"
+                        />
+                        <rect
+                          x="225.28"
+                          y="102.394"
+                          width="20.48"
+                          height="81.92"
+                        />
+                        <rect
+                          x="102.4"
+                          y="286.714"
+                          width="20.48"
+                          height="81.92"
+                        />
+                        <path
+                          d="M385.761,307.2c1.126-1.004,3.359-4.526,3.359-10.24c0-6.287-2.724-9.912-3.666-10.465l-133.325,0.184
+			                  	c0.737,6.82,0.737,13.722,0.021,20.521H385.761z"
+                        />
+                        <path
+                          d="M409.6,358.4c0-6.287-2.724-9.912-3.666-10.465l-17.592,0.021c-0.614,0.021-1.167,0.205-1.782,0.205H272.609
+				c0.717,6.799,0.737,13.701,0.02,20.48h14.09h119.521C407.368,367.636,409.6,364.114,409.6,358.4z"
+                        />
+                        <polygon
+                          points="347.501,94.71 328.966,103.414 363.823,177.551 382.357,168.847 			"
+                        />
+                        <path
+                          d="M471.04,0H20.48C9.155,0,0,9.175,0,20.48v163.84h61.44V81.92c0-11.305,9.155-20.48,20.48-20.48h61.44
+				c11.325,0,20.48,9.175,20.48,20.48h20.48c0-11.305,9.155-20.48,20.48-20.48h61.44c10.691,0,19.169,8.253,20.111,18.678
+				c1.802-2.048,4.014-3.809,6.656-5.038l55.583-26.153c10.24-4.833,22.426-0.41,27.279,9.83l52.265,111.186
+				c2.212,4.69,2.273,9.748,0.922,14.377h62.464V20.48C491.52,9.175,482.365,0,471.04,0z"
+                        />
+                        <path
+                          d="M81.92,409.6H0v61.44c0,11.305,9.155,20.48,20.48,20.48h163.84V409.6h-40.96H81.92z"
+                        />
+                        <path
+                          d="M307.2,409.6v81.92h163.84c11.325,0,20.48-9.175,20.48-20.48V409.6h-84.48H307.2z"
+                        />
+                        <polygon
+                          points="286.72,184.326 321.741,184.326 286.72,109.84 			"
+                        />
+                        <rect
+                          x="163.84"
+                          y="122.874"
+                          width="20.48"
+                          height="61.44"
+                        />
+                        <rect
+                          x="102.4"
+                          y="102.394"
+                          width="20.48"
+                          height="81.92"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                </svg>
+                <span
+                  class="ms-1 m-text-small d-flex justify-content-center align-items-center"
+                  >Your library</span
+                >
+              </div>
+              <div class="d-flex mt-4">
+                <i class="mid-gray-text bi bi-plus-square m-bi-plus-square"></i>
+                <span
+                  class="mid-gray-text ms-1 m-text-small d-flex justify-content-center align-items-center"
+                  >Create playlist</span
+                >
+              </div>
+              <div class="d-flex mt-1 mid-gray-text">
+                <svg
+                  fill="white"
+                  width="16px"
+                  height="16px"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 1000 1000"
+                  enable-background="new 0 0 1000 1000"
+                  xml:space="preserve"
+                >
+                  <metadata>
+                    Svg Vector Icons : http://www.onlinewebfonts.com/icon
+                  </metadata>
+                  <g>
+                    <g
+                      transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                    >
+                      <path
+                        d="M560.3,5010.4c-172.6-42.2-329.9-172.6-412.3-337.5L100,4577V118.1v-4458.9l47.9-95.9c59.5-122.7,174.5-237.8,297.3-297.2l95.9-47.9H5000h4458.9l95.9,47.9c122.7,59.4,237.8,174.5,297.3,297.2l47.9,95.9V118.1V4577l-47.9,95.9c-59.4,122.7-174.5,237.8-297.3,297.3l-95.9,47.9l-4420.5,3.8C2606.6,5021.9,592.9,5018.1,560.3,5010.4z M4039.2,2446.3c297.3-67.1,565.8-214.8,805.5-441.1l164.9-157.3l153.4,147.7c379.7,364.4,868.8,537,1363.6,481.4c843.8-94,1482.5-761.4,1536.2-1603.3c23-375.9-130.4-736.4-494.8-1168c-71-84.4-677-700-1344.4-1365.5l-1214-1214L3730.4-1592.6c-703.8,703.8-1306,1313.7-1340.5,1355.9c-197.5,251.3-345.2,529.3-404.7,763.3c-32.6,128.5-38.4,191.8-28.8,339.5c46,717.3,508.2,1319.4,1181.4,1536.2C3437,2498.1,3745.8,2513.4,4039.2,2446.3z"
+                      />
+                    </g>
+                  </g>
+                </svg>
+                <span
+                  class="ms-1 mb-4 -text-small d-flex justify-content-center align-items-center"
+                  >Liked songs</span
+                >
+              </div>
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 1</span
+              >
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 2</span
+              >
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 3</span
+              >
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 4</span
+              >
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 5</span
+              >
+              <div class="d-flex align-items-baseline">
+                <span class="m-truncate ml-0 mt-1 m-text-smaller dark-gray-text"
+                  >One hell of a song name</span
+                ><i
+                  class="bi bi-people-fill m-bi-people-fill dark-gray-text ms-1"
+                ></i>
+              </div>
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 6</span
+              >
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 7</span
+              >
+              <div class="d-flex align-items-baseline">
+                <span class="m-truncate ml-0 mt-1 m-text-smaller dark-gray-text"
+                  >One hell of a song name</span
+                ><i
+                  class="bi bi-people-fill m-bi-people-fill dark-gray-text ms-1"
+                ></i>
+              </div>
+              <span class="ml-0 mt-1 m-text-smaller dark-gray-text"
+                >Song 8</span
+              >
+              <div class="d-flex align-items-baseline">
+                <span class="m-truncate ml-0 mt-1 m-text-smaller dark-gray-text"
+                  >One hell of a song name</span
+                ><i
+                  class="bi bi-people-fill m-bi-people-fill dark-gray-text ms-1"
+                ></i>
+              </div>
+              <div class="d-flex align-items-baseline">
+                <span class="m-truncate ml-0 mt-1 m-text-smaller dark-gray-text"
+                  >One hell of a song name</span
+                ><i
+                  class="bi bi-people-fill m-bi-people-fill dark-gray-text ms-1"
+                ></i>
+              </div>
+              <div class="d-flex align-items-baseline">
+                <i
+                  class="bi bi-arrow-down-circle m-bi-arrow-down-circle-fill dark-gray-text me-1"
+                ></i>
+                <span class="fw-bold ml-0 mt-1 m-text-smaller dark-gray-text"
+                  >Install app</span
+                >
+              </div>
+            </aside>
+          </div>
+
+          <!-- main section -->
+          <div class="col-10">
+            <section>
+              <!-- main content of the page -->
+              <div class="container">
+                <div class="h6">main content</div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+      <!-- music player section -->
+
+      <section>
+        <div
+          class="container-fluid m-music-player-section-height d-flex justify-content-between"
+        >
+          <!-- left side music player-->
+
+          <div class="d-flex align-items-center">
+            <img src="https://picsum.photos/60" />
+            <p class="text-white m-text-small fw-bold ms-3 mb-1">
+              Another One Bite The Dust - Remastered 2011 <br />
+              <span class="dark-gray-text m-text-small">Queen</span>
+            </p>
+            <i class="bi bi-heart m-bi-heart text-white ms-3"></i>
+            <i class="bi bi-tv m-bi-tv text-white ms-3"></i>
+          </div>
+
+          <!-- mid music player -->
+          <div class="d-flex flex-column mt-2">
+            <div class="d-flex justify-content-center align-items-center">
+              <i class="bi bi-shuffle m-bi-shuffle me-2 text-white"></i>
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                class="me-2"
+                fill="white"
+                width="24px"
+                height="24px"
+                x="0px"
+                y="0px"
+                viewBox="0 0 1000 1000"
+                enable-background="new 0 0 1000 1000"
+                xml:space="preserve"
+              >
+                <metadata>
+                  Svg Vector Icons : http://www.onlinewebfonts.com/icon
+                </metadata>
+                <g>
+                  <g transform="matrix(1 0 0 -1 0 960)">
+                    <path
+                      d="M232.7-30c-12.5,0-23,4.3-31.6,12.9c-8.6,8.6-12.9,19.2-12.9,31.6v890.9c0,12.5,4.3,23,12.9,31.6c8.6,8.6,19.2,12.9,31.6,12.9h89.1c12.5,0,23-4.3,31.6-12.9c8.6-8.6,12.9-19.2,12.9-31.6V515.2l413.4,403.6c7.7,6.5,14,9.8,18.7,9.8c8.9,0,13.4-7.7,13.4-23.2V14.5c0-15.4-4.5-23.2-13.4-23.2c-4.8,0-11,3.3-18.7,9.8L366.4,404.8V14.5c0-12.5-4.3-23-12.9-31.6c-8.6-8.6-19.2-12.9-31.6-12.9H232.7z"
+                    />
+                  </g>
+                </g>
+              </svg>
+              <i
+                class="bi bi-play-circle-fill m-bi-play-circle-fill me-2 text-white"
+              ></i>
+              <svg
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                class="me-2"
+                fill="white"
+                width="24px"
+                height="24px"
+                x="0px"
+                y="0px"
+                width="439.357px"
+                height="439.357px"
+                viewBox="0 0 439.357 439.357"
+                style="enable-background: new 0 0 439.357 439.357"
+                xml:space="preserve"
+              >
+                <g>
+                  <path
+                    d="M360.427,5.833c-3.613-3.617-7.898-5.424-12.847-5.424h-36.545c-4.945,0-9.233,1.807-12.847,5.424
+                c-3.614,3.621-5.428,7.902-5.428,12.85v193.574c-0.948-2.091-2.19-3.901-3.717-5.424L86.343,4.125
+                c-3.617-3.617-6.663-4.856-9.136-3.713c-2.474,1.143-3.711,4.189-3.711,9.137v420.262c0,4.948,1.237,7.994,3.711,9.138
+                c2.473,1.14,5.519-0.1,9.136-3.724L289.048,232.52c1.529-1.711,2.765-3.521,3.72-5.424V420.66c0,4.948,1.81,9.239,5.421,12.847
+                c3.62,3.62,7.904,5.428,12.847,5.428h36.552c4.948,0,9.236-1.808,12.847-5.428c3.614-3.607,5.428-7.898,5.428-12.847V18.683
+                C365.854,13.73,364.037,9.45,360.427,5.833z"
+                  />
+                </g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+              </svg>
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                class="me-2"
+                fill="white"
+                width="24px"
+                height="24px"
+                x="0px"
+                y="0px"
+                viewBox="0 0 472.615 472.615"
+                style="enable-background: new 0 0 472.615 472.615"
+                xml:space="preserve"
+              >
+                <g>
+                  <g>
+                    <path
+                      d="M355.232,0l-13.525,13.525l65.821,65.821h-279.17c-52.894,0-95.924,43.031-95.924,95.919v59.633h19.128v-59.633
+                  c0-42.343,34.452-76.79,76.796-76.79h279.17l-65.821,65.821l13.525,13.525l88.91-88.91L355.232,0z"
+                    />
+                  </g>
+                </g>
+                <g>
+                  <g>
+                    <path
+                      d="M421.053,237.714v59.632c0,42.344-34.452,76.795-76.796,76.795H65.087l65.821-65.825l-13.525-13.525l-88.909,88.914
+                  l88.909,88.91l13.525-13.525L65.087,393.27h279.17c52.895,0,95.924-43.031,95.924-95.924v-59.632H421.053z"
+                    />
+                  </g>
+                </g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+              </svg>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+              <span class="mid-gray-text me-3">0.25</span>
+              <span class="mid-gray-text d-block m-music-player1"></span>
+              <span class="mid-gray-text d-block m-music-player2"></span>
+              <span class="mid-gray-text ms-3">0.59</span>
+            </div>
+          </div>
+          <!-- right side music player -->
+          <div class="d-flex align-items-center">
+            <i class="bi bi-music-note-list mid-gray-text me-3"></i>
+            <i class="bi bi-pc-display mid-gray-text me-3"></i>
+            <i class="bi bi-volume-up mid-gray-text ms-3"></i>
+            <span class="mid-gray-text d-block m-music-player3"></span>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+    <script src="homepage.js" defer></script>
+  </body>
+</html>
